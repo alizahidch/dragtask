b436f5c3fc5ef0533978b48d9962ebd990d6f6c8
alizahidch <you@example.com>
2020-04-26 14:43:23 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray,transferArrayItem} from '@angular/cdk/drag-drop';
+import { splitAtColon } from '@angular/compiler/src/util';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dragTask';
+  firstTask = [
+    'Div 1 ',
+    'Div 2 '
+  
+  ];
+
+
+
+
+  secondTask=[
+    'Div 1 ',
+    'Div 2 ',
+    'Div 3 ',
+    'Div 4 '
+
+  ]
+
+  drop(event: CdkDragDrop<string[]>) {
+
+    moveItemInArray(this.firstTask, event.previousIndex, event.currentIndex);
+    let initialIndex=event.previousIndex;
+    let divName='Div '+initialIndex.toString();
+    this.firstTask.splice(event.previousIndex,0,divName)
+   
+  }
+
+
+
+  dropSecond(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.secondTask, event.previousIndex, event.currentIndex);
+    // this.secondTask.splice(event.currentIndex,0,this.secondTask[event.previousIndex])
+    let initialIndex=event.previousIndex;
+    let finalIndex=event.currentIndex;
+console.log(initialIndex)
+console.log(finalIndex)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import bootstrap from "bootstrap";
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    BrowserAnimationsModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
